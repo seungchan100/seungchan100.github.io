35deaedea3b14a65bf7de76857c13865931d7ceb
seungchan.100 <seungchan.100@gmail.com>
2022-04-29 08:30:48 +0900

    init

---
+++ pages/products/simpleLedger.vue
@@ -0,0 +1,176 @@
+<template>
+  <v-container class="mt-10 pl-10 pr-10" style="border:solid #666;border-width:8px 0 0 0">
+      <v-row class="pt-5 pb-5">
+        <v-col cols="8" class="pl-5">
+          <v-row class="text-h4">SeungChan baek</v-row>
+          <v-row class="text-h7">Software Engineer</v-row>
+        </v-col>
+        <v-col cols="4">
+            <br/>
+            seungchan.100@gmail.com
+        </v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row class="pt-5 pb-5">
+        <v-col cols=2 class="text-h7 font-italic font-weight-bold">
+          Profile
+        </v-col>
+        <v-col class="md-8" cols=10>
+          Software engineer at Samsung Electronics. <br>
+          Experienced various software develop environment(desktop app, web, mobile, etc) since 2010. <br>
+          Not only Interested in Programming, but also DevOps, Refactoring, and Finance.
+        </v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row class="pt-5 pb-5">
+        <v-col cols=2 class="text-h7 font-italic font-weight-bold">
+          Skills
+        </v-col>
+        <v-col cols=3>
+          <p class="text-h7 font-weight-bold">Programming</p>
+          I can make Application program in Unix,  Windows,  Web, and Mobile environment.
+          Having various knowledge and experiences of OS/Web. Also having basic skill about mobile programming.
+        </v-col>
+        <v-col cols=3>
+          <p class="text-h7 font-weight-bold">Software Design</p>
+          I have ability to design software considering various develop environment by using data structures, algorithms
+          and illustrate by using diagrams (Sequence Diagram, ER Diagram, etc.)
+          Having UX-friendly design ability and communication skills for clients.
+        </v-col>
+        <v-col cols=3>
+          <p class="text-h7 font-weight-bold">Analyze Legacy System</p>
+          I have been participated in maintenance of legacy softwares.
+          In order to fix bugs and improve systems, can analyze by using some debug / profiling software like dbx, gdb, wireshark, etc.
+          Also, I have knowledge of operating systems, network, security.
+        </v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row class="pt-5 pb-5">
+        <v-col cols=2 class="text-h7 font-italic font-weight-bold">
+          Technicals
+        </v-col>
+        <v-col cols=10>
+          <v-row>
+            <span class="font-weight-bold">Language</span> : C, C#, Java, Javascript, PL/SQL, PHP, Python
+          </v-row>
+          <v-row>
+            <span class="font-weight-bold">Framework</span> : Spring, Mybatis, .NET(C#), Node.js, Ionic, Vue.js, Nuxt.js
+          </v-row>
+          <v-row>
+            <span class="font-weight-bold">etc</span> : Windows/Linux server maintenance, React-native, Jenkins, Git, Tmax, Tomcat, httpd
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row class="pt-5 pb-5">
+        <v-col cols=2 class="text-h7 font-italic font-weight-bold">
+          Experience
+        </v-col>
+        <v-col cols=10>
+          <v-row v-for="(item, index) in experience"
+            :key="index"
+            v-bind:class="{'pt-1':true, 'pb-2':true, 'border-bottom':index != experience.length-1}">
+            <v-col>
+              <v-row class="font-weight-bold text-h7">
+                {{item.company}}
+              </v-row>
+              <v-row class="text-caption mt-2">
+                {{item.title}}
+              </v-row>
+              <v-row class="pt-1 text-body-2">
+                <span v-for="(item2, index2) in item.description" :key="index2">
+                - {{item2}}
+                </span>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row class="pt-5 pb-5">
+        <v-col cols=2 class="text-h7 font-italic font-weight-bold">
+          Education
+        </v-col>
+        <v-col cols=10>
+          <v-row>
+            <v-col>
+              <v-row class="font-weight-bold text-h7">
+                Korea University (Seoul, Korea)
+              </v-row>
+              <v-row class="text-caption mt-2">
+                Bachelor of Computer Science (2010.03~2016.02)
+              </v-row>
+              <v-row class="pt-1 text-body-2">
+                <span>- Member of Kweb(web programming club) and chairman at 2012 and studied about HTML / CSS / Javscript / PHP / MySQL / Webservers.</span>
+                <span>- Learned : Artificial Intelligence, Data Science, Distributed System, DataBase System, Operating System, Compiler, Data Communication, etc.</span>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row class="pt-5 pb-5">
+        <v-col cols=2 class="text-h7 font-italic font-weight-bold">
+          Projects
+        </v-col>
+        <v-col cols=10>
+          <v-btn to="/projects" block>link</v-btn>
+        </v-col>
+      </v-row>
+
+</v-container>
+</template>
+
+<style>
+.border-bottom:{
+  border-bottom:1px solid rgba(0,0,0,0.12);
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      experience:[
+        {
+          company:'Samsung Electronics',
+          title : 'Software Engineer (2017.08~)',
+          description:[
+            'Participated in maintenance and improvement of EES (Equipment Engineering System, Semiconductor Equipment Management System).'
+            ,'Developed and Designed new features of EES Client program and Web server by C#(.NET, WCF, WPF).'
+            ,'Implement Non-Failover by using distributed system algorithms.'
+            ,'Improvement chart rendering performance (90% improvement on 1 million data)'
+          ]
+        },
+        {
+          company:'TmaxSoft/TmaxCloud',
+          title:'Software Engineer (2016.02~2017.06)',
+          description:
+            [
+              'Developed, designed data collector module of APM(Application Performance Management) Solution.'
+              ,'Experienced in programming multi-therad programming / IPC (file, signal, socket, shared memory, etc) on HPUX, AIX, Solaris, Linux.'
+            ]
+        },
+        {
+          company:'Gamess',
+          title : 'Technical Engineer (2011.02~)',
+          description : [
+            'Gamess is game community website(gamess.co.kr) having about 2 million members.'
+            ,'Participated in developing Gamess, I developed membership features (user home, like, friend, login, etc) by using PHP.'
+            ,'I have maintained, operated, developed Gamess servers(Windows/Linux) since 2011.'
+          ]
+        },
+        {
+          company: 'FreeLancer',
+          title : 'Software Engineer (2011.02~2016.02)',
+          description : [
+            'Participated in a lot of software and improved software development skills like Java, PL/SQL, Spring, Node.js, Ionic, etc. mostly web projects.'
+            ,'Projects : Korea University IRB system, Korea University Open API, Hantech Media - History Management System, (Spring, Mybatis, PHP, Javascript, HTML)'
+            +'Websites on Dept. of Brain of Korea University (PHP, Javascript, HTML),  Qplace analyze System(PHP, HTML, Javascript), etc'
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/projects.vue
@@ -0,0 +1,195 @@
+<template>
+  <v-container>
+      <v-row class="mt-2">
+        <v-col>
+          <v-row class="">
+            Technicals <!-- <v-chip x-small class="mt-1 ml-1">All</v-chip> -->
+          </v-row>
+          <v-row class="mt-3" v-for="(value, key) in technicals" :key="key">
+            {{key}} : <v-chip class="mt-1 ml-1" x-small v-for="(item, index) in value" :key="index">{{item}}</v-chip>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row class="mt-5" align="left">
+        <v-card v-for="(item, index) in projects" :key="index"
+          class="mx-auto mt-1"
+          max-width="280px"
+          outlined
+        >
+          <v-img
+            v-if="item.thumbnail.length>0"
+            v-bind:src="item.thumbnail[0]"
+            height="200px"
+            style="cursor:pointer;"
+            @click="popup(item)"
+          ></v-img>
+          <v-card-text>
+            <p class="subtitle-1 text--primary font-weight-black">
+              {{item.title}}
+            </p>
+            <div class="text--primary">
+              {{item.desc}}
+            </div>
+            <v-chip x-small class="mr-1" v-for="(item2, index2) in item.tags" :key="index2">
+              {{item2}}
+            </v-chip>
+          </v-card-text>
+        </v-card>
+      </v-row>
+      <v-overlay
+        z-index="9"
+        :value="overlay"
+        @click.native="overlay = false"
+      >
+        <v-img
+          z-index="10"
+          contain
+          height="500px"
+          :src="overlayImage"
+        ></v-img>
+      </v-overlay>
+</v-container>
+</template>
+
+<script>
+//포트폴리오 참고 http://findmatthew.com/
+export default{
+  methods:{
+    popup(item){
+      this.overlay = true;
+      this.overlayImage = item.image[0];
+    },
+  },
+  data(){
+    return {
+      overlay:false,
+      overlayImage:'',
+      technicals:{
+        "Language" : ["C", "C#", "Java", "PHP", "Python", "Javascript", "SQL"],
+        "Framework" : ["Spring", ".NET(C#)", "Node.js", "Ionic", "Vue.js"],
+        "Etc" : ["React-native", "Android", "Jenkins", "Tmax"],
+      },
+      projects:[
+        {
+          title:"scsoft android app (2019.06~)",
+          desc:"https://play.google.com/store/apps/developer?id=scsoft. Developed and published 4 apps of scsoft : Stock Miner, Tag Note, Simple Ledger, Country Flag Quiz."
+          + " Each apps were made by ionic framework. Some apps are Vue.js based, some apps are Angular.js based.",
+          tags:["Ionic","Vue.js","Angular.js", "Javascript"],
+          image:['/projects/scsoft_android.png'],
+          thumbnail:['/projects/thumbnail/thumbnail_scsoft_android.png'],
+        },
+        {
+          title:"Samsung Electronics EES UI 2.0 Client / Webserver (2017.08~)",
+          desc:"In Samsung Electronics (semiconductor), my job is maintaining MES-EES(Equipment Engineering System) Client / Web server. "
+          + "Have made each module's UI. (Recipe Management System, Fault Detection System, Process Control System, etc)"
+          +"Plus, developed Software load Balnacer that is not using H/W Load Balancer, improved chart render performance (over 90% speed up) by algorithm utilization,"
+          +" introduced DevOps : Git, Jenkins, Crucible/Fisheye, Jira. In order to patch and verify patch/setup system, I developed patch script and program.",
+          tags:["WCF","WPF","C#(.NET)"],
+          image:[],
+          thumbnail:[],
+        },
+        {
+          title:"TmaxSoft Sysmaster (2016.02~2017.06)",
+          desc:"When I was in Tmaxsoft / TmaxCloud corporation, I was member of 'Sysmaster' APM solution.(Application Performance Management.)"
+            +"My job is made / maintain Data collector module which measure server / webtoB(webserver), tmax(TP monitor), proframe(tmax framework),"
+            +"Oracle(DB), Anylink(interface framework) and send data to central server."
+            +" Developed in unix environment(HPUX, Linux, AIX, Solaris) by using C or Java programming."
+            +" Experienced POSIX system call, concurrent program, multi thread-process architecture, socket programming, "
+            +"IPC(file, pipe, signal, shared memory), etc.",
+          tags:["C", "Java"],
+          image:['/projects/sysmaster.gif'],
+          thumbnail:['/projects/thumbnail/thumbnail_sysmaster.png'],
+        },
+        {
+          title:"Low fertility and Aging society web site (2016.02)",
+          desc:"This website is based on korea - egov framework (fork from spring framework)."
+                +"I participated in this project as Backend programmer and frontend javascript programmer."
+                +"developed Member function(register, withdrawl, modification)," 
+                +"Board function(view, modify, write, delete, reply, attach file). "
+                +"In order to make many boards, I generalized several codes / classes and designed by using design pattern."
+                +"Additional, I participated in front-end javascript programmer for devloping api communicaion and front end action/animation",
+          tags:["Spring","Mybatis", "Java", "Javascript"],
+          image:['/projects/precap.png'],
+          thumbnail:['/projects/thumbnail/thumbnail_precap.png'],
+        },
+        {
+          title:"Korea University Open API (2015.12~2016.02)",
+          desc:"In Korea University Information and Computing Office, I participated in developing Korea University Open API. "
+          + "Because most members didn't know about spring/mybatis, I taught spring / mybatis / MVC pattern."
+          + " And I made architecture of open API base and developed common code.",
+          tags:["Spring","Mybatis", "Java"],
+          image:['/projects/koreauniv_openapi.png'],
+          thumbnail:['/projects/thumbnail/thumbnail_koreauniv_openapi.png'],
+        },
+        {
+          title:"Qplace (2013.06~2014.08)",
+          desc:"Qplace was restaurant recommend application. I participated in developed in Android application, and developed IOS application ported from Android app."
+          + "In addition, I developed tools for user action analysis system by using PHP and HTML, Javascript, Bootstrap. ",
+          tags:["Android", "objective-c", "PHP", "Javascript"],
+          image:['/projects/qplace.png'],
+          thumbnail:['/projects/thumbnail/thumbnail_qplace.png'],
+        },
+        {
+          title:"RapaEdu (2013.06~08)",
+          desc:"Rapaedu is company that provide video lecture. I developed homework / review feature in this company by using APS.NET, SQL, front-end."
+            +" Also, maintained all of this company's website and web server and DB.",
+          tags:["ASP.NET", "SQL", "Javascript"],
+          image:['/projects/rapaedu.png'],
+          thumbnail:['/projects/thumbnail/thumbnail_rapaedu.png'],
+        },
+        {
+          title:"Gomav (2013.06~07)",
+          desc:"recommend friend's gift website. Developed website (server / client)",
+          tags:["PHP", "Javascript"],
+          image:['/projects/gomav.png'],
+          thumbnail:['/projects/thumbnail/thumbnail_gomav.png'],
+        },
+        {
+          title:"Autocall System (2013.04~08)",
+          desc:"Developed website and android application. First, insert telephone number of people into DB by website."
+            +" After then, User using android app can call each person automatically by communicating with web server.",
+          tags:["PHP", "SQL", "Javascript", "Java","Android"],
+          image:['/projects/autocall_app.jpg'],
+          thumbnail:['/projects/thumbnail/thumbnail_autocall_app.png'],
+        },
+        {
+          title:"Hyundai Steel BFG system (2013.02~03)",
+          desc:"Developed simulation UI program calculating gas / fluid values by Using 'Flow Master' C# library.",
+          tags:["C#"],
+          image:['/projects/BFG system.png'],
+          thumbnail:['/projects/thumbnail/thumbnail_BFG system.png'],
+        },
+        {
+          title:"Hantech Media History Management System (2012.11~12)",
+          desc:"Developed server / client side by using Spring Framework, MyBatis, MSSQL",
+          tags:["Spring", "Java", "Mybatis", "SQL", "Javascript"],
+          image:['/projects/HTM HMS.png'],
+          thumbnail:['/projects/thumbnail/thumbnail_HTM HMS.png'],
+        },
+        {
+          title:"Korea University IRB mobile web (2012.09~10)",
+          desc:"Developed some pages of server side logic by using spring framework, ibatis.",
+          tags:["Spring","Java", "Mybatis", "SQL"],
+          image:['/projects/koreauniv_irb.png'],
+          thumbnail:['/projects/thumbnail/thumbnail_koreauniv_irb.png'],
+        },
+        {
+          title:"Korea University Brain dept. websites (2012.02~03, 2015.03~06)",
+          desc:"made / designed / maintained SMC2012, Werob2015, BEC2015 websites.",
+          tags:["Javascript"],
+          image:['/projects/smc2012.png'],
+          thumbnail:['/projects/thumbnail/thumbnail_smc2012.png'],
+        },
+        {
+          title:"GameSS (2011.02~)",
+          desc:"GameSS (www.gamess.co.kr) is Korean Game community site having 2 million members. I have been Technical Engineer since 2011."
+            +" Developed social network(user's home, board, like, point, etc functions) and maintain servers.",
+          tags:["Javascript","PHP"],
+          image:['/projects/gamess.png'],
+          thumbnail:['/projects/thumbnail/thumbnail_gamess.png'],
+        },
+      ]
+    }
+  }
+}
+</script>
